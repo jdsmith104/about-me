7f856fa431445a4631f008502cf1c57b4d9a822c
DESKTOP-GKLETRF\jdsmi <jdsmith104@outlook.com>
2022-04-30 18:48:15 +0100

    Applied prettier and started adding end points

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,10 +1,14 @@
-import * as functions from "firebase-functions";
+/*
+ *  POST: /add-project - add project to db
+ *  GET: /all-projects: - get all projects
+ *  GET: /project?id=<id>
+ *
+ *
+ */
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+import * as functions from 'firebase-functions'
+import * as express from 'express'
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
-
+const app = express()
+app.get('/', (req, res) => res.status(200).send('Hey there!'))
+exports.app = functions.https.onRequest(app)
