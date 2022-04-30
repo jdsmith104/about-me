55e8dc4c7b7ba59e48fb2fc9b57aa5ba20bc660d
DESKTOP-GKLETRF\jdsmi <jdsmith104@outlook.com>
2022-04-30 19:15:35 +0100

    Continued with guide to create a post method

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -6,9 +6,12 @@
  *
  */
 
-import * as functions from 'firebase-functions'
-import * as express from 'express'
+import * as functions from "firebase-functions";
+import * as express from "express";
+import { addEntry } from './entryController'
 
-const app = express()
-app.get('/', (req, res) => res.status(200).send('Hey there!'))
-exports.app = functions.https.onRequest(app)
+const app = express();
+
+app.post('/entries', addEntry)
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+exports.app = functions.https.onRequest(app);
