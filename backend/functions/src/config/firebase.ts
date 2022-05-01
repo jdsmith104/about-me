bc408c396f0fe20fed38bc171d6918afde29f52b
jdsmith104 <jdsmith104@outlook.com>
2022-05-01 20:00:46 +0100

    Ran prettier script

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -2,12 +2,12 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
-        projectId: functions.config().project.id,
-        clientEmail: functions.config().client.email,
-    }),
-    databaseURL: "https://jacobdsmith-81e81.firebaseio.com",
+  credential: admin.credential.cert({
+    privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
+    projectId: functions.config().project.id,
+    clientEmail: functions.config().client.email,
+  }),
+  databaseURL: "https://jacobdsmith-81e81.firebaseio.com",
 });
 
 const db = admin.firestore();
--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -8,10 +8,10 @@
 
 import * as functions from "firebase-functions";
 import * as express from "express";
-import { addEntry } from './entryController'
+import { addEntry } from "./entryController";
 
 const app = express();
 
-app.post('/entries', addEntry)
+app.post("/entries", addEntry);
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 exports.app = functions.https.onRequest(app);
