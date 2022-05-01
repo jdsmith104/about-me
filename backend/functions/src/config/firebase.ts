00417e7565faa3b7072cee33daca70b366ebcd15
jdsmith104 <jdsmith104@outlook.com>
2022-05-01 20:07:05 +0100

    Updated config to follow firebase v10 expectations found here: https://firebase.google.com/docs/admin/setup?authuser=1. This uses the recommended GOOGLE_APPLICATION_CREDENTIALS

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -1,12 +1,9 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
+import { applicationDefault } from "firebase-admin/app";
+
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
-    projectId: functions.config().project.id,
-    clientEmail: functions.config().client.email,
-  }),
+  credential: applicationDefault(),
   databaseURL: "https://jacobdsmith-81e81.firebaseio.com",
 });
 
