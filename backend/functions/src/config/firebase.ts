5a36c637eb1c59615f611bdbe34caa17202e5b6d
DESKTOP-GKLETRF\jdsmi <jdsmith104@outlook.com>
2022-07-07 10:35:36 +0100

    Added jest for testing

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -1,7 +1,6 @@
 import * as admin from "firebase-admin";
 import { applicationDefault } from "firebase-admin/app";
 
-
 admin.initializeApp({
   credential: applicationDefault(),
   databaseURL: "https://jacobdsmith-81e81.firebaseio.com",
