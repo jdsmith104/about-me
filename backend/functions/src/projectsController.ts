551be6e8ede25646fcbf875dc33705538a03f52a
jdsmith104 <jdsmith104@outlook.com>
2022-07-08 10:31:31 +0100

    Updated API to return specific projects whe requested

--- backend/functions/src/projectsController.ts
+++ backend/functions/src/projectsController.ts
@@ -39,13 +39,25 @@ const postProject = async (req: Request, res: Response) => {
   }
 };
 
-const getProjects = async (req: Request, res: Response) => {
+const getProjects = async (req: any, res: Response) => {
   try {
     const snapshot = await db.collection("projects").get();
 
+    const projectID: string = req.query?.id;
+    let data;
+    if (projectID) {
+      console.log("Hiya", projectID);
+      const filteredQuery = snapshot.query.where("id", "==", projectID);
+      const querySnapshot = await filteredQuery.get();
+      data = querySnapshot.docs[0].data();
+      console.log(querySnapshot.size, data);
+    } else {
+      data = snapshot.docs.map((doc) => doc.data());
+    }
+
     res.status(200).send({
       status: "success",
-      data: snapshot.docs.map((doc) => doc.data()),
+      data: data,
     });
   } catch (error: any) {
     res.status(500).json(error.message);
