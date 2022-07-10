443a645b1f0a55d97c01d797bb6b8d7562ff9500
jdsmith104 <jdsmith104@outlook.com>
2022-07-10 11:58:12 +0100

    Updated test file to work with new api

--- backend/functions/src/projectsController.test.ts
+++ backend/functions/src/projectsController.test.ts
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 
-const baseUrl = "http://localhost:5001/jacobdsmith-81e81/us-central1/app";
-
 test("test server live", async () => {
-  const response = await fetch(baseUrl, {
-    method: "GET",
-  });
+  const response = await fetch(
+    "http://localhost:5001/jacobdsmith-81e81/us-central1/app",
+    {
+      method: "GET",
+    }
+  );
   const status = response.status;
   const statusOK = response.ok;
   expect(status).toBe(200);
@@ -16,21 +17,22 @@ describe("Make post request and get all projects", () => {
   const bodyData = {
     title: "Demo Project",
     icon: "https://play-lh.googleusercontent.com/1nfAdJs2Ep2q1skM7QwJ1uHooWSbpFkbIBHhAX6EmdzEKmtk42713TiTU28mWlkcFKPA=w480-h960",
-    technology: ["C++", "Qt5"],
+    technologies: ["C++", "Qt5"],
     tagline:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit posuere volutpat. Duis cursus condimentum massa, ac fringilla metus. Mauris tempor lorem nec mattis lacinia. Etiam aliquet felis sit amet lectus mattis, quis porttitor metus suscipit. Maecenas pulvinar tempor lacus, eu pharetra lacus pulvinar quis. Fusce ultricies augue quis diam.",
     href: "https://github.com/jdsmith104/about-me",
   };
   test("post project successful", async () => {
-    const projectsUrl = baseUrl + "/projects";
-
-    let response = await fetch(projectsUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyData),
-    });
+    let response = await fetch(
+      "http://localhost:5001/jacobdsmith-81e81/us-central1/addProject",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+      }
+    );
 
     const status = response.status;
     const statusOK = response.ok;
@@ -40,11 +42,12 @@ describe("Make post request and get all projects", () => {
   });
 
   test("get projects request successful", async () => {
-    const projectsUrl = baseUrl + "/projects";
-
-    const response = await fetch(projectsUrl, {
-      method: "GET",
-    });
+    const response = await fetch(
+      "http://localhost:5001/jacobdsmith-81e81/us-central1/getProjects",
+      {
+        method: "GET",
+      }
+    );
     const responseBody: any = await response.json();
     // Array of project documents
     const projects: Array<any> = responseBody.data;
@@ -66,24 +69,25 @@ describe("Make post request and get specific project", () => {
     const bodyData = {
       title: "",
       icon: "https://play-lh.googleusercontent.com/1nfAdJs2Ep2q1skM7QwJ1uHooWSbpFkbIBHhAX6EmdzEKmtk42713TiTU28mWlkcFKPA=w480-h960",
-      technology: ["C++", "Qt5"],
+      technologies: ["C++", "Qt5"],
       tagline:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit posuere volutpat. Duis cursus condimentum massa, ac fringilla metus. Mauris tempor lorem nec mattis lacinia. Etiam aliquet felis sit amet lectus mattis, quis porttitor metus suscipit. Maecenas pulvinar tempor lacus, eu pharetra lacus pulvinar quis. Fusce ultricies augue quis diam.",
       href: "https://github.com/jdsmith104/about-me",
     };
 
-    const projectsUrl = baseUrl + "/projects";
-
     for (let index: number = 0; index < 3; index++) {
       bodyData.title = projectTitlePrefix + index.toString();
 
-      const response = await fetch(projectsUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(bodyData),
-      });
+      const response = await fetch(
+        "http://localhost:5001/jacobdsmith-81e81/us-central1/addProject",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bodyData),
+        }
+      );
 
       const status = response.status;
       const statusOK = response.ok;
@@ -96,7 +100,9 @@ describe("Make post request and get specific project", () => {
   });
 
   test("get specific project by id", async () => {
-    const projectsUrl = baseUrl + "/projects?id=" + projectIDs[0];
+    const projectsUrl =
+      "http://localhost:5001/jacobdsmith-81e81/us-central1/getProjects?id=" +
+      projectIDs[0];
 
     const response = await fetch(projectsUrl, {
       method: "GET",
