7183d5f55c9712eb79fa484194fff0b8a4502c66
jdsmith104 <jdsmith104@outlook.com>
2022-07-10 09:55:01 +0100

    Added cors package to allow requests from front-end. GET requests are now valid, but going to refactor code to be a more traditional firebase style

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -9,8 +9,12 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import { getProjects, postProject } from "./projectsController";
+const cors = require("cors");
 
 const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
 app.post("/projects", postProject);
 app.get("/projects", getProjects);
